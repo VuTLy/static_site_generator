dfa519539c997f15469877bf05c52a9646bea3fa
VuTLy <vulyccschool@example.com>
2025-03-14 21:51:22 -0700

    7: up to date after got it delete

--- src/main.py
+++ src/main.py
@@ -1,13 +1,26 @@
-from textnode import *
-from htmlnode import *
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+from gencontent import generate_pages_recursive
+
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
+
 
 def main():
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-    test = TextNode('This is some anchor text', TextType.LINK , 'https://www.boot.dev')
-    print(test)
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
-    node = HTMLNode(props={"href": "https://example.com", "class": "link"})
-    print(node.props_to_html())
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
-if __name__ == "__main__":
-    main()
+
+main()
